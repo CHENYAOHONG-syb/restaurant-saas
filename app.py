df7710c628cf8a05e570f6a40e8f34198b115df4
曾耀宏 <chenyaohong@cengyaohongdeMacBook-Air.local>
2026-03-12 09:13:56 +0800

    fix railway deploy

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def add_food():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
